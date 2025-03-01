e3b08ecbc293c4d5eac99d353624c27b868c0cd7
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2025-02-28 21:13:55 -0800

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Products from './pages/Products';
-import Process from './pages/Process';
-import Facilities from './pages/Facilities';
-import Careers from './pages/Careers';
-import Contact from './pages/Contact';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Process from "./pages/Process";
+import Facilities from "./pages/Facilities";
+import Careers from "./pages/Careers";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
